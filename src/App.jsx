5dc0f7cab13315217858d047a287a1c3acea2219
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-07-06 12:59:56 -0300

    att

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import avatarHref from './assets/imgs/user-avatar.png';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-window">
+      <div className="sidebar">
+        <header>
+          <img className="header--avatar" src={avatarHref} alt="Imagem do usuário" />
+          <div className="header--buttons"></div>
+        </header>
+
+        <div className="search">
+          search
+        </div>
+
+        <div className="chatlist">
+          chats
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="contentarea">
+        opened chat
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
