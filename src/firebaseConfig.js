c3ca0687a11957cdf16dc1f17fe34afd8b4556b1
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-07-09 20:33:16 -0300

    project finished

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,7 +1,7 @@
 export default {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
+  projectId: `${import.meta.env.VITE_PROJECT_ID}`,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
